a461dc1c593e10ce40e1870d0baf93e30c4a5f35
raochunjiang <287573755@qq.com>
2017-09-29 09:52:42 +0800

    添加实例项目

---
+++ EFCoreSamples/EFCoreSamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
